89288073f275eaec40a16952987fe28fba9ac40d
afaq <mafaqq318@gmail.com>
2023-09-04 13:18:46 +0300

    Added Level1 Security

--- app.js
+++ app.js
@@ -1,1 +1,82 @@
-//jshint esversion:6
+const express= require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+    extended:true
+}));
+
+mongoose.connect('mongodb://127.0.0.1:27017/userDB', {useNewUrlParser:true});
+
+const UserSchema={
+    email: String,
+    password: String
+}
+
+const User = mongoose.model('User', UserSchema);
+
+
+app.get("/",function(req,res){
+    res.render("home");
+})
+
+app.get("/login",function(req,res){
+    res.render("login");
+})
+
+app.get("/register",function(req,res){
+    res.render("register");
+})
+
+app.post("/register",async function (req,res){
+    const newUser= new User ({
+        email: req.body.username,
+        password: req.body.password
+    });
+
+   
+    try {
+        await newUser.save();
+        console.log("New User added");
+        res.render("secrets")
+        } 
+    catch (err) {
+        console.log(err);
+        }
+    
+});
+
+app.post("/login",async function (req,res){
+    const username=req.body.username;
+    const password=req.body.password;
+
+   
+try {
+        const FoundUser= await User.findOne({email: username});
+
+        if(FoundUser){
+            if(FoundUser.password===password){
+                res.render("secrets");
+            }
+            else{
+                res.render("login");
+            }
+        }
+        else{
+            res.render("login");
+        }
+        } 
+    catch (err) {
+        console.log(err);
+        }
+    
+});
+
+const PORT=3000;
+app.listen(PORT, function() {
+    console.log("server started on port:"+ PORT);
+});
