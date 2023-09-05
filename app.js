4b31bb430ae3058f18c81c0923f567a6189e2192
afaq <mafaqq318@gmail.com>
2023-09-05 13:24:27 +0300

    Added Oauth Google.

--- app.js
+++ app.js
@@ -6,10 +6,9 @@ const mongoose = require("mongoose");
 const session = require('express-session')
 const passport = require('passport');
 const passportLocalMongoose = require('passport-local-mongoose');
-
-
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate =  require('mongoose-findorcreate')
 const app = express();
-const saltRounds=10;
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -30,22 +29,51 @@ mongoose.connect('mongodb://127.0.0.1:27017/userDB', {useNewUrlParser:true});
 
 const UserSchema= new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId: String,
 });
 
 UserSchema.plugin(passportLocalMongoose);
+UserSchema.plugin(findOrCreate);
 
 const User = mongoose.model('User', UserSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done) {
+    done(null, user);
+  });
+passport.deserializeUser(function(user, done) {
+done(null, user);
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+    User.findOrCreate({ email: profile.emails[0].value, googleId: profile.id }, function (err, user) { 
+     return cb(err, user) 
+    });
+}
+));
 
 
 app.get("/",function(req,res){
     res.render("home");
 })
+
+app.get("/auth/google", passport.authenticate('google', { scope: ['profile',"email"] }));
+
+app.get('/auth/google/secrets', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/secrets');
+  });
 
 app.get("/login",function(req,res){
     res.render("login");
